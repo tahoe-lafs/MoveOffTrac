0712e6093e00f68c1cc47ed997733321446eadef
Benoit Donneaux <benoit@leastauthority.com>
2024-07-25 08:33:46 +0200

    Remove unused part of the helper
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- helpers/trac_dump.sh
+++ helpers/trac_dump.sh
@@ -7,24 +7,13 @@ set -o pipefail # exit on any failed command in pipes
 
 # Verbosity settings
 : ${VERBOSE=1}
-SH=("/usr/bin/env" "bash")
 if [ ${VERBOSE} -ge 2 ]; then
-  SH=("${SH[@]}" "-x")
   set -o xtrace
 fi
-
-# Set magic variables for current file & dir
-__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__FILE="${__DIR}/$(basename "${BASH_SOURCE[0]}")"
-__BASE="$(basename ${__FILE} .sh)"
-__ROOT="$(cd "$(dirname "${__DIR}")" && pwd)"
-__CWD="$(pwd)"
 
 # Parse the arguments
 TRAC_DIR=${1?Path to trac directory is missing as first argument!}
 DUMP_DIR=${2?Path to dump directory is missing as second argument!}
-shift
-shift
 
 trac-admin "${TRAC_DIR}" hotcopy "${DUMP_DIR}"
 
