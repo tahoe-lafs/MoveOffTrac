4d5152449248edaf71781425a61b03229ef1d1d5
Benoit Donneaux <benoit@leastauthority.com>
2024-10-11 17:37:08 +0200

    More Gandi records after zone creation
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- terraform/core/dns_tl-org.tf
+++ terraform/core/dns_tl-org.tf
@@ -1,3 +1,4 @@
+# Manage the domain
 resource "gandi_domain" "tl-org" {
   name = "tahoe-lafs.org"
   owner {
@@ -20,4 +21,34 @@ resource "gandi_domain" "tl-org" {
     type            = "association"
     zip             = "12345"
   }
+}
+
+# Accessing domain data
+data "gandi_domain" "tl-org" {
+  name = gandi_domain.tl-org.name
+}
+
+# Managing DNS zone
+resource "gandi_livedns_domain" "tl-org" {
+  name = data.gandi_domain.tl-org.name
+  timeouts {
+    default = "3600s"
+  }
+}
+
+# Managing the NS records of the zone
+resource "gandi_nameservers" "tl-org" {
+  domain      = data.gandi_domain.tl-org.name
+  nameservers = data.gandi_domain.tl-org.nameservers
+}
+
+# Managing other records of the zone
+resource "gandi_livedns_record" "tl-org_A" {
+  name = "@"
+  type = "A"
+  values = [
+    "1.2.3.4"
+  ]
+  ttl  = 3600
+  zone = data.gandi_domain.tl-org.name
 }
